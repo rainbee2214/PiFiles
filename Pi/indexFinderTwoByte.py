7e5d39bf5155a772ec31d6b9ee36d64a3b9e23d7
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-05-10 22:33:42 -0300

    Split into two scripts

--- Pi/indexFinderTwoByte.py
+++ Pi/indexFinderTwoByte.py
@@ -3,24 +3,24 @@ import itertools
 from time import time
 
 validHex = '0123456789ABCDEF'
-repeat = 3
 
-threeBit = itertools.product(validHex, repeat=repeat)
+repeat = 4
+fourBit = itertools.product(validHex, repeat=repeat)
 foundIndices = []
 
 start = time()
 
-for x in threeBit:
+for x in fourBit:
     # print "Searching for", x
     found = False
     index = 0
     while not found:
         test = pi.getHexFromIndex(index)
-        if x[0] == test[0] and x[1] == test[1] and x[2] == test[2]:
+        if x[0] == test[0] and x[1] == test[1] and x[2] == test[2] and x[3] == test[3]:
             # print "Match!", x, test, index
             found = True
             foundIndices.append((x, index))
-        elif x[0] == test[2] and x[1] == test[1] and x[2] == test[0]:
+        elif x[0] == test[3] and x[1] == test[2] and x[2] == test[1] and x[3] == test[0]:
             found = True
             foundIndices.append((x, str(index)+'R'))
         else:
@@ -28,7 +28,7 @@ for x in threeBit:
 
 end = time()
 
-print "three took", (end - start)/60, ":", (end - start)%60
+print "four took", (end - start)/60, ":", (end - start)%60
 
-with open("01halfByte.dat", 'w') as f:
+with open("02Byte.dat", 'w') as f:
     data = f.write('\n'.join('%s %s' % x for x in foundIndices))
