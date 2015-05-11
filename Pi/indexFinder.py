86434d5a42fdfa3d4c406f9d6a8d11e8c88b4f9a
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-05-10 22:27:48 -0300

    Fix index finder, add another test for server

--- Pi/indexFinder.py
+++ Pi/indexFinder.py
@@ -1,28 +1,64 @@
 import pi
 import itertools
+from time import time
 
 validHex = '0123456789ABCDEF'
 repeat = 3
 
-twoBit = itertools.product(validHex, repeat=repeat)
+threeBit = itertools.product(validHex, repeat=repeat)
 foundIndices = []
 
-for x in twoBit:
-	# print "Searching for", x
-	found = False
-	index = 0
-	while not found:
-		test = pi.getHexFromIndex(index)
-		if x[0] == test[0] and x[1] == test[1] and x[2] == test[3]:
-			# print "Match!", x, test, index
-			found = True
-			foundIndices.append((x, index))
-		elif x[0] == test[2] and x[1] == test[1] and x[2] == test[0]:
-			found = True
-			foundIndices.append((x, str(index)+'R'))
-		else:
-			index += 1
+start = time()
 
-with open("output" + str(repeat), 'w') as f:
-        data = f.write('\n'.join('%s %s' % x for x in foundIndices))
+for x in threeBit:
+    # print "Searching for", x
+    found = False
+    index = 0
+    while not found:
+        test = pi.getHexFromIndex(index)
+        if x[0] == test[0] and x[1] == test[1] and x[2] == test[2]:
+            # print "Match!", x, test, index
+            found = True
+            foundIndices.append((x, index))
+        elif x[0] == test[2] and x[1] == test[1] and x[2] == test[0]:
+            found = True
+            foundIndices.append((x, str(index)+'R'))
+        else:
+            index += 1
 
+end = time()
+
+print "three took", (end - start)/60, ":", (end - start)%60
+
+with open("01halfByte.dat", 'w') as f:
+    data = f.write('\n'.join('%s %s' % x for x in foundIndices))
+
+
+repeat = 4
+fourBit = itertools.product(validHex, repeat=repeat)
+foundIndices = []
+
+start = time()
+
+for x in fourBit:
+    # print "Searching for", x
+    found = False
+    index = 0
+    while not found:
+        test = pi.getHexFromIndex(index)
+        if x[0] == test[0] and x[1] == test[1] and x[2] == test[2] and x[3] == test[3]:
+            # print "Match!", x, test, index
+            found = True
+            foundIndices.append((x, index))
+        elif x[0] == test[3] and x[1] == test[2] and x[2] == test[1] and x[3] == test[0]:
+            found = True
+            foundIndices.append((x, str(index)+'R'))
+        else:
+            index += 1
+
+end = time()
+
+print "four took", (end - start)/60, ":", (end - start)%60
+
+with open("02Byte.dat", 'w') as f:
+    data = f.write('\n'.join('%s %s' % x for x in foundIndices))
