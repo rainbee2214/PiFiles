3f2e7d8944ba119418e9c5907114bf36a1f9bbbd
Sarah <rainbee2214@gmail.com>
2015-05-16 08:37:29 -0300

    Added SMS options.

--- Server/Pi/cliMessage.py
+++ Server/Pi/cliMessage.py
@@ -2,12 +2,12 @@ import sys
 import coder
 
 def main(argv):
-    script, flag, message = sys.argv
+    script, flag, sendToSMS, message = sys.argv
 
     if flag == '-e':
-        coder.encodeMessage(message,  coder.getPiWithTwoHex(), 2)
+        coder.encodeMessage(message,  coder.getPiWithTwoHex(), 2, sendToSMS)
     elif flag == '-d':
-        coder.decodeMessage(message,  coder.getPiWithTwoHex(), 2)
+        coder.decodeMessage(message,  coder.getPiWithTwoHex(), 2, sendToSMS)
 
 
 if __name__ == "__main__":
